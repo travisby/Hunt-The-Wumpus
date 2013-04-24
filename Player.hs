7b328507cf27ee85a6b3f1e3f1c5eb6903ea352f
Travis Beatty <travisby@gmail.com>
2013-04-24 14:01:32 -0400

    Added simple smell function for the player

--- Player.hs
+++ Player.hs
@@ -10,6 +10,12 @@ data Player = Player {
 playerStart :: Player
 playerStart = Player 3 0
 
+smell gameMap wumpus
+    | 1 <= filter (==wumpusLocation) adjacentRooms  = "I smell something..."
+    | otherwise = ""
+    where
+        wumpusLocation = location wumpus
+        adjacentRooms = getAdjacentRoomIDs gameMap
 -- smell
 -- touch
 -- hear
