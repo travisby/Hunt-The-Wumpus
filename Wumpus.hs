5153d82507fd0eb8a495c2b7a66757cf7cefb066
Travis Beatty <travisby@gmail.com>
2013-05-01 20:02:20 -0400

    Created a move function for the wumpus

--- Wumpus.hs
+++ Wumpus.hs
@@ -9,4 +9,8 @@ data Wumpus = Wumpus {
     room    :: Room
 }
 
+move newSpot wumpus gameMap
+    | isAdjacentToCreature newSpot (room wumpus) gameMap = Wumpus newSpot
+    | otherwise = error "Cannot move into that room"
+
 getMoveForWumpus wumpus gameMap = safeChoice (getPossibleRooms gameMap (room wumpus))
