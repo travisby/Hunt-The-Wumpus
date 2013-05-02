99f4a7aa7b0dea374d4aceb5c94807ad12ffbe39
Travis Beatty <travisby@gmail.com>
2013-05-01 20:15:00 -0400

    Created a function to see if the wumpus and player were adjacent

--- State.hs
+++ State.hs
@@ -6,12 +6,16 @@ import Wumpus
 import Map
 
 data State = State {
-    player  :: Player,
-    wumpus  :: Wumpus,
-    map     :: Map
+    player      :: Player,
+    wumpus      :: Wumpus,
+    ourGameMap  :: Map
 }
 
 start playerStart wumpusStart = State (Player playerStart 3) (Wumpus wumpusStart)
     where gameMap = getMap 20
 
-areThePlayerAndTheWumpusAdjacent (State player wumpus gameMap) = areTwoRoomsAdjacent (Player.room player) (Wumpus.room wumpus) gameMap
+areThePlayerAndTheWumpusAdjacent state = areTwoRoomsAdjacent (Player.room ourPlayer) (Wumpus.room ourWumpus) gameMap
+    where
+        ourPlayer = player state
+        ourWumpus = wumpus state
+        gameMap = ourGameMap state
