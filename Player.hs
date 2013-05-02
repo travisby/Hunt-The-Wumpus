3e9371960e6d617df3dc2b2cb387bf85c32be337
Travis Beatty <travisby@gmail.com>
2013-05-01 20:23:32 -0400

    added a function to shoot for players

--- Player.hs
+++ Player.hs
@@ -13,3 +13,7 @@ data Player = Player {
 move newSpot player gameMap
     | isAdjacentToCreature newSpot (room player) gameMap = Player newSpot (numArrows player)
     | otherwise = error "Cannot move into that room"
+
+shoot spot player gameMap
+    | isAdjacentToCreature spot (room player) gameMap = Player (room player) (numArrows player - 1)
+    | otherwise = error "Cannot shoot into that room"
