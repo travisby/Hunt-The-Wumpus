c875d9e7c774ceebce382a79ca49fc3727ecf0c9
Travis Beatty <travisby@gmail.com>
2013-05-07 21:26:47 -0400

    Added a gameLoop function just to ask for a move

--- Game.hs
+++ Game.hs
@@ -1,10 +1,16 @@
 import State
 import Map
 
+gameLoop state = do
+    putStrLn $ "[move/shoot] " ++ State.getPossibleRoomsStr state
+    playerMove <- getLine
+    return playerMove
+
 main = do
     let myMap = Map.getMap 20
     startRoomForPlayer <- Map.getRandomPointOnMap myMap
     startRoomForWumpus <- Map.getRandomPointOnMap myMap
     let state = State.start startRoomForPlayer startRoomForWumpus myMap
-
+    gameLoop state
+    -- 1 for failure, because not finished :P
     putStr $ show 1
