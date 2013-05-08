d625ba04945b41cb4c6279baf5772b04f5ee802e
Travis Beatty <travisby@gmail.com>
2013-05-07 23:03:51 -0400

    Simplistic game loop

--- Game.hs
+++ Game.hs
@@ -2,11 +2,25 @@ import State
 import Map
 
 gameLoop state = do
+    putStrLn $ State.playerSmell state
     putStrLn "[move/shoot] "
     playerMove <- getLine
     putStrLn $ "Room " ++ State.getPossibleRoomsStr state
     playerRoom <- getLine
-    return playerMove
+
+    let newState = State.makeMoveFromStr playerMove playerRoom state
+
+    let isThereAWinner = State.winning newState
+    if isThereAWinner == 0
+        then gameLoop newState
+    else if isThereAWinner == 1
+        -- my way of exiting :P
+        then error "You lost!"
+    else if isThereAWinner == 2
+        -- my way of exiting :P
+        then error "Congratulations, you won!"
+    else error "Woah that function was weird"
+
 
 main = do
     let myMap = Map.getMap 20
